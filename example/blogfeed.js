264f220fe4e8567a54a0e571343e779e805bdd50
lucyc <lucy@machinemode.com>
2015-04-21 16:32:06 -0400

    Added stylesheet

--- example/blogfeed.js
+++ example/blogfeed.js
@@ -16,21 +16,20 @@
 	//Load the Google Feed API if not already loaded 
 	if (!google.feeds) google.load("feeds", "1");
 
-	$.fn.blogFeed = function(options){
+	$.fn.blogFeed = function(options) {
 		
-		// Create some defaults, extending them with any options that were provided
+		// Defaults, extended by any options that were provided
 		var settings = $.extend( {
 			'maxWords'		: 55,
 			'feedPath'		: 'blog',
 			'feedQuantity'	: 3,
-			'feedImage'		: 0
-		},options);
+			'feedImage'		: 0 // 0 (No Image) | 1 (Image)
+		}, options);
 		
 		var imgBox = "";
 		
 		function getImage(entryContent){
-			//Check for image 
-			//this is based on UVM blog rss, where images are included in entry.content (Not in MediaGroup)
+			//Check for image - based on blog.uvm.edu rss, where images are included in entry.content (Not in MediaGroup)
 			if($("img", entryContent).attr("src")){
 				imgBox = '<img src="'
 				+ $("img", entryContent).attr("src") 
@@ -40,8 +39,7 @@
 			}
 		}
 		
-		function trimWords(theString, maxWords) {
-			
+		function trimWords( theString, maxWords ) {	
 			if (theString.match(/\[...\]$/))
 			{
 				//If a Feed Excerpt ( ends with [...]) leave as-is
@@ -53,59 +51,57 @@
 			}
 		}
 		
-		return this.each(function() {
+		return this.each( function() {
 			var $this = $(this);	
 				
 			function initialize() {
-					// Create a feed instance that will grab the feed
-					var feed = new google.feeds.Feed(settings.feedPath);
-					
-					// Set the number of feed entries
-					feed.setNumEntries(settings.feedQuantity);
-				   
-					// Calling load sends the request off (It requires a callback function)
-					feed.load(function(result) {
-				 		
-						var outputHtml = "";
-						
-						if (!result.error) {
-							var container = ""; 
-						   	for (var i = 0; i < result.feed.entries.length; i++) {  
-								var entry = result.feed.entries[i];  
-								var d = new Date(entry.publishedDate); //e.g. Wed Nov 28 2012 12:14:25 GMT-0500 (Eastern Standard Time)
+			// Create a feed instance that will grab the feed
+				var feed = new google.feeds.Feed(settings.feedPath);
 				
-								var pubDate = "";
-								if (!isNaN(d.getMonth())){
-									pubDate = (d.getMonth()+1) + '-' + d.getDate() + '-' + d.getFullYear();// Format as: MM-DD-YYYY (e.g. 11-28-2012)
-								} 
-								else {
-									return false;
-								}
+				// Set the number of feed entries
+				feed.setNumEntries(settings.feedQuantity);
+			   
+				// Calling load sends the request off (It requires a callback function)
+				feed.load( function( result ) {
+			 		
+					var outputHtml = "";
+					
+					if ( !result.error ) {
+						var container = ""; 
+					   	for (var i = 0; i < result.feed.entries.length; i++) {  
+							var entry = result.feed.entries[i];  
+							var d = new Date(entry.publishedDate); //e.g. Wed Nov 28 2012 12:14:25 GMT-0500 (Eastern Standard Time)
+			
+							var pubDate = "";
+							if (!isNaN(d.getMonth())){
+								pubDate = (d.getMonth()+1) + '-' + d.getDate() + '-' + d.getFullYear();// Format as: MM-DD-YYYY (e.g. 11-28-2012)
+							} 
+							else {
+								return false;
+							}
+							
+							if(settings.feedImage === 1){
+								getImage(entry.content);
+							}
 								
-								if(settings.feedImage === 1){
-									getImage(entry.content);
-								}
-									
-								outputHtml += '<article class="clearfix">'
-											+ '<h4><a href="' + entry.link + '" target="_blank" >' +  entry.title + '</a></h4>' 
-											+  imgBox
-											+ '<p><span class="feed-date" style="color: #999">' + pubDate  + '</span> ' +  trimWords(entry.content, settings.maxWords) + ' '
-											+ '<a class="feed-more" href="' + entry.link + '" target="_blank" >Read more &raquo;</a></p>'  
-											+ '</article>';
-						}  
-						} 
-						else {  
-							alert("Error fetching feeds!")
-							outputHtml += '<p><a target="_blank" href="' + settings.feedPath + '" >Check out our blog »</a></p>';  
-						}  
-					  
-						$this.html(outputHtml);       
-		   			});
-				}
-		  
-				//load callback function
-				google.setOnLoadCallback(initialize);
-
+							outputHtml += '<article class="clearfix">'
+										+ '<h1><a href="' + entry.link + '" target="_blank" >' +  entry.title + '</a></h1>' 
+										+  imgBox
+										+ '<p><span class="feed-date" style="color: #999">' + pubDate  + '</span> ' +  trimWords(entry.content, settings.maxWords) + ' '
+										+ '<a class="feed-more" href="' + entry.link + '" target="_blank" >Read more &raquo;</a></p>'  
+										+ '</article>';
+					}  
+					} 
+					else {  
+						alert("Error fetching feeds!")
+						outputHtml += '<p><a target="_blank" href="' + settings.feedPath + '" >Check out our blog »</a></p>';  
+					}  
+				  
+					$this.html(outputHtml);       
+	   			});
+			}  
+			//load callback function
+			google.setOnLoadCallback(initialize);
 		});	
 	};
 	
